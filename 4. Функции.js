29585151052645b169bc960f86180567031a2fd5
olegnuzhnenko <82514158+olegnuzhnenko@users.noreply.github.com>
2022-08-22 16:45:46 +0200

    Add files via upload

--- 4. Функции.js
+++ 4. Функции.js
@@ -20,3 +20,47 @@
 
 //Задание 3
 // Напишите функцию repeatWord(word, count), которая принимает слово и количество раз для его повторения. Например, если вызвать repeatWord('слово', 3) функция напечатает "слово1, слово2, слово3" на одной строке.
+
+// function repeatWord(word, count) {
+// 	let words = ''
+//   for (let i = 1; i <= count; ++i) {
+//   	words += word + i + ', '
+//   }
+//   alert(words);
+// }
+
+// repeatWord('слово', 3)
+
+//Задание 4
+
+//Напишите функцию createAdder(a), которая возвращает функцию addA, которая принимает b и возвращает a + b.
+
+// Функции должны принимать аргументы.
+
+// function createAdder(a) {
+// 	return function addA(b) {
+//     return a + b;
+//   }
+// }
+
+// const add5 = createAdder(5);
+// alert( add5(5) ) // Должно получиться 10
+// alert( add5(12) ) // Должно получиться 17
+
+//Напишите функцию getMonth(n), которая принимает номер месяца и возвращает его название. Например, getMonth(12) ⇒ 'декабрь'. Используйте if/else
+
+// function getMonth(n){
+//     if (n === 1) return 'JAN'
+//     if (n === 2) return 'FEB'
+//     if (n === 3) return 'MAR'
+//     if (n === 4) return 'APR'
+//     if (n === 5) return 'MAY'
+//     if (n === 6) return 'JUN'
+//     if (n === 7) return 'JUL'
+//     if (n === 8) return 'AUG'
+//     if (n === 9) return 'SEP'
+//     if (n === 10) return 'OCT'
+//     if (n === 11) return 'NOV'
+//     if (n === 12) return 'DEC'
+// }
+// alert(getMonth(3))
